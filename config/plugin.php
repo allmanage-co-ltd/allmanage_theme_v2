7b331ab33aef18df8bbd3ff6306b19ec335cf5b9
gucchi0421 <gucchi.1999.1121.0421@gmail.com>
2026-06-18 11:58:42 +0900

    s

---
+++ config/plugin.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+  /**
+   * app/Hooks/SetupTheme.php > adminNoticeRequiredPlugins() で参照
+   *
+   * - name: プラグインの名称
+   * - slug: プラグインのファイルパス（フォルダ名/ファイル名.php）
+   * - repo: wordpress.org のプラグインスラッグ
+   */
+  [
+    'name' => 'SiteGuard WP Plugin',
+    'slug' => 'siteguard/siteguard.php',
+    'repo' => 'siteguard',
+  ],
+  [
+    'name' => 'WPvivid Backup Plugin',
+    'slug' => 'wpvivid-backuprestore/wpvivid-backuprestore.php',
+    'repo' => 'wpvivid-backuprestore',
+  ],
+  [
+    'name' => 'All in One SEO',
+    'slug' => 'all-in-one-seo-pack/all_in_one_seo_pack.php',
+    'repo' => 'all-in-one-seo-pack',
+  ],
+  [
+    'name' => 'MW WP Form',
+    'slug' => 'mw-wp-form/mw-wp-form.php',
+    'repo' => 'mw-wp-form',
+  ],
+  [
+    'name' => 'Advanced Custom Fields',
+    'slug' => 'advanced-custom-fields/acf.php',
+    'repo' => 'advanced-custom-fields',
+  ],
+  [
+    'name' => 'Post Types Order',
+    'slug' => 'post-types-order/post-types-order.php',
+    'repo' => 'post-types-order',
+  ],
+  [
+    'name' => 'Category Order and Taxonomy Terms Order',
+    'slug' => 'taxonomy-terms-order/taxonomy-terms-order.php',
+    'repo' => 'taxonomy-terms-order',
+  ],
+];
