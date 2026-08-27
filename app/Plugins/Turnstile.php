4d349c5cb4631577b71bbacb11c4ced8a43bd4f1
gucchi0421 <gucchi.1999.1121.0421@gmail.com>
2026-08-27 12:31:07 +0900

    debug(turnstile): デバッグ出力を画面表示に変更（一時）

--- app/Plugins/Turnstile.php
+++ app/Plugins/Turnstile.php
@@ -165,7 +165,7 @@ class Turnstile implements BootableWpHookInterface
    */
   private function validateSessionOnComplete(mixed $Validation, mixed $Data, string $session_key): mixed
   {
-    \error_log('[Turnstile] complete check. session_id=' . \session_id() . ' key=' . $session_key . ' session=' . \print_r($_SESSION[$session_key] ?? 'NONE', true));
+    echo '<pre style="background:#fff;color:#000;padding:10px;z-index:9999;position:fixed;top:0;left:0;right:0">[Turnstile complete] session_id=' . \session_id() . ' key=' . $session_key . ' data=' . \print_r($_SESSION[$session_key] ?? 'NONE', true) . '</pre>';
 
     $session = $_SESSION[$session_key] ?? [];
 
@@ -225,7 +225,7 @@ class Turnstile implements BootableWpHookInterface
       'verified_at' => \time(),
     ];
 
-    \error_log('[Turnstile] confirm verified. session_id=' . \session_id() . ' key=' . $session_key . ' session=' . \print_r($_SESSION[$session_key], true));
+    echo '<pre style="background:#fff;color:#000;padding:10px;z-index:9999;position:fixed;top:0;left:0;right:0">[Turnstile confirm] session_id=' . \session_id() . ' key=' . $session_key . ' data=' . \print_r($_SESSION[$session_key], true) . '</pre>';
 
     return $Validation;
   }
@@ -285,7 +285,7 @@ class Turnstile implements BootableWpHookInterface
     );
     $session_key = self::SESSION_PREFIX . \md5($validation_key);
 
-    \error_log('[Turnstile] blockMail check. session_id=' . \session_id() . ' validation_key=' . $validation_key . ' session_key=' . $session_key . ' session=' . \print_r($_SESSION[$session_key] ?? 'NONE', true));
+    echo '<pre style="background:#fff;color:#000;padding:10px;z-index:9999;position:fixed;top:0;left:0;right:0">[Turnstile blockMail] session_id=' . \session_id() . ' validation_key=' . $validation_key . ' session_key=' . $session_key . ' data=' . \print_r($_SESSION[$session_key] ?? 'NONE', true) . '</pre>';
 
     if (!empty($_SESSION[$session_key]['verified'])) {
       return $Mail;
